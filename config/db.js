a160de66ef0f25c14a3279210c0fecdf2f9dcfc5
fallenAfter <brandon@Brandons-MacBook-Pro.local>
2016-04-14 16:32:04 -0400

    sending local files to get after backup

---
+++ config/db.js
@@ -0,0 +1,9 @@
+// create public connection string
+module.exports = {
+    // local
+    // 'url': 'mongodb://localhost/test'
+
+    // live on mlab
+    'url': 'mongodb://crimeUmbrella:team16@ds023118.mlab.com:23118/crime-umbrella'
+};
+//ds023118.mlab.com:23118/crime-umbrella -u -p
---
+++ routes/auth.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+
+// add auth package refs
+var passport = require('passport');
+var mongoose = require('mongoose');
+var Account = require('../models/account');
+var configDb = require('../config/db.js');
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+    Account.findById(id, function(err, user){
+        done(err,user);
+    });
+});
+
+// GET register - show registration form
+router.get('/register', function(req,res,next) {
+    res.render('auth/register', {
+        title: 'Register'
+    });
+});
+// POST register - save new user
+router.post('/register', function(req, res, next) {
+    /* Try to create a new account using our Account model & the form values
+     * If we get an error, display the register form again
+     * If registration works, store the user and show the articles main page */
+    Account.register(new Account(
+        { username: req.body.username, name: req.body.name, adminLevel: req.body.adminLevel }), req.body.password, function(err, account) {
+        if (err) {
+            res.render('auth/register', {
+                title: 'Register'
+            });
+            console.log(err);
+        }
+        else {
+            req.login(account, function(err) {
+                res.redirect('login');
+            });
+        }
+    });
+});
+// GET login - show login form
+router.get('/login', function(req,res,next) {
+    // store the session messages in a local variable
+    var messages = req.session.messages || [];
+    // clear the session messages
+    req.session.messages = [];
+    // show the login page and pass in any messages we may have
+    res.render('auth/login', {
+        title: 'Login',
+        user: req.user,
+        messages: messages
+    });
+    // clear out session message after
+    //req.session.messages = [];
+});
+// POST login - validate user
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/auth/login',
+    failureMessage: 'Invalid Login'
+}));
+
+//make this public
+module.exports = router, passport;
